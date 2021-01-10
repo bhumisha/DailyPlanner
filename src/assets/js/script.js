7769ac47e41848cc136ae1b487005ad406030f8b
Bhumisha Dave <bhumishadave@Bhumishas-MBP.hsd1.ca.comcast.net>
2021-01-10 13:16:23 -0800

    added comment and current day as header

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,90 +1,121 @@
 
-
-var todayDateStatTime  = moment('08:00:00',"h");
-var todayDateEndTime  = moment('17:00:00',"h");
+//Declaration of dates. taking start and end time.
+var todayDateStatTime  = moment().hour(8);
+var todayDateEndTime  = moment().hour(17);
 var timeFormat = 'hA';
 
+//Time difference for time block 
 var timeDiff = todayDateEndTime.diff(todayDateStatTime,'h');
-var timeToshow = todayDateStatTime;
+var todoListHour = todayDateStatTime;
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+//Rander Today Calander will rander all time block
+randerDayPlanner();
 
-console.log(todayDateStatTime);
-randerTodayCal();
 
-var taskList = JSON.parse(localStorage.getItem('todoTaskList')) || [];
-
-function randerTodayCal(){
+//Rander Today Calander will rander all time block
+function randerDayPlanner(){
 
     for(var i=0; i<timeDiff; i++)
     {
         var divrow = $('<div>');
         var divcolTime = $('<div>');
-        var todoTask = $('<textarea>');
-        var btnTaskSave = $('<button>');
+        var taTodoTask = $('<textarea>');
+        var btnTodoTaskSave = $('<button>');
         var iforSavebtn = $('<i>');
     
+        //Applying css class to the elements.
         divrow.addClass('row time-block');
         divcolTime.addClass('col-1 hour d-flex align-items-center');
-        todoTask.addClass('col-10 description');
-        btnTaskSave.addClass('col-1 saveBtn btn');
+        taTodoTask.addClass('col-10 description');
+        btnTodoTaskSave.addClass('col-1 saveBtn btn');
         iforSavebtn.addClass('fa fa-save');
         
-        btnTaskSave.append(iforSavebtn);
+        //Increment of hour by 1 with given 'hA' formate.
+        var timeToShow = todoListHour.add(1,"hours").format(timeFormat);
 
-        var timeShow = moment(timeToshow.add(1,"H")).format(timeFormat)
-
-        divcolTime.append(timeShow);
-        divcolTime.attr("time-calc",timeToshow.format("HH"));
-        btnTaskSave.attr("task-time",timeShow);
-        divrow.attr("id",timeShow);
+        divcolTime.append(timeToShow);
+        btnTodoTaskSave.attr("task-time",timeToShow);
+        divrow.attr("id",timeToShow);
         
+        //Add Future/pass and preset time block CSS based on current time.
         var currentHour = moment().hour();
-        if(currentHour == moment(timeToshow).hour())
+        if(moment(todoListHour).hour() === currentHour)
         {
-            todoTask.addClass('present');
+            taTodoTask.addClass('present');
         }
-        else if(currentHour > moment(timeToshow).hour())
+        else if(moment(todoListHour).hour() < currentHour)
         {
-            todoTask.addClass('past');   
+            taTodoTask.addClass('past');   
         }
         else
         {
-            todoTask.addClass('future');
+            taTodoTask.addClass('future');
         }   
 
-        divrow  = divrow.append(divcolTime);
-        divrow  = divrow.append(todoTask);
-        divrow  = divrow.append(btnTaskSave);
+        divrow.append(divcolTime);
+        divrow.append(taTodoTask);
+        btnTodoTaskSave.append(iforSavebtn);
+        divrow.append(btnTodoTaskSave);
 
         $(".container").append(divrow);
     }
-   renderTodos();
+    //Load Task Description from Localstorage.
+   loadSavedTodos();
 }
-function renderTodos() {
+//Rander saved tasks  from Localstorage.
+function loadSavedTodos() {
+
+    //Reading localstorage list if any
     var taskList = JSON.parse(localStorage.getItem('todoTaskList')) || [];
     if(taskList){
         for(var i=0;i<taskList.length;i++)
         {
-            var taskTime = taskList[i].time;
+            var taskTime = taskList[i].tasktime;
             var timeRow = $('#'+taskTime);
             timeRow.children('textarea').val(taskList[i].taskDesc);
    
         }
     }
 }
+
+//Save Button Click for each Task.
 $(document).on('click', '.saveBtn', function(event) {
     event.preventDefault();
     
+    //Reading localstorage list if any
     var taskList = JSON.parse(localStorage.getItem('todoTaskList')) || [];
-    
-    var time =  $(this).attr("task-time");
-    var todoTaskDesc =  $(this).siblings(".description").val().trim();
-    var task = { time: time, taskDesc :todoTaskDesc};
 
-    taskList.push(task);
+    //get data from current click element
+    var time =  $(this).attr("task-time"); //Save button attr to get time.
+    var todoTaskDesc =  $(this).siblings(".description").val().trim(); //get textArea - sibling to get description value
     
+    var isTaskExist = false;
+   
+    for(var i=0;i<taskList.length;i++)
+    {
+        if(time === taskList[i].tasktime){
+            isTaskExist = true;
+            if(todoTaskDesc === ""){
+                taskList.splice(i,1);
+                break;
+            }
+            else{
+                taskList[i].taskDesc = todoTaskDesc;
+            }
+        }
+    }
+    if(!isTaskExist){
+        //Create object and add to localstorage.
+        if(todoTaskDesc === ""){
+            alert("Please enter todo task");
+        }
+        else{
+            var task = { tasktime: time, taskDesc :todoTaskDesc};
+            taskList.push(task);
+        }
+    }
+
     localStorage.setItem('todoTaskList',JSON.stringify(taskList));
-    
-   renderTodos(taskList);
-    
+   
   });
 
